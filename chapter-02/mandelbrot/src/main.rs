f96d10ca119a4c509b63c47e0a8ba24e01cdc617
Andrew Jones <ascjones@gmail.com>
2017-05-20 08:38:09 +0100

    implement single threaded main

--- chapter-02/mandelbrot/src/main.rs
+++ chapter-02/mandelbrot/src/main.rs
@@ -1,9 +1,54 @@
 extern crate num;
+extern crate image;
 
+use std::io::Write;
+use std::fs::File;
+use std::str::FromStr;
 use num::Complex;
+use image::ColorType;
+use image::png::PNGEncoder;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(std::io::stderr(), 
+                 "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT")
+            .unwrap();
+        writeln!(std::io::stderr(), 
+                 "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+                 args[0])
+            .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parssing image dimensions");
+    let upper_left = parse_pair(&args[3], ',')
+        .expect("error parsing upper left corer point");
+    let lower_right = parse_pair(&args[4], ',')
+        .expect("error parsing lower right corner point");
+    
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_bitmap(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
+}
+
+/// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
+/// file named `filename`.
+fn write_bitmap(filename: &str, pixels: &[u8], bounds: (usize, usize))
+    -> Result<(), std::io::Error>
+{
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels,
+                   bounds.0 as u32, bounds.1 as u32,
+                   ColorType::Gray(8))?;
+    Ok(())
 }
 
 /// Render a rectangle of the Mandelbrot set into a buffer of pixels.
@@ -78,4 +123,35 @@ fn test_pixel_to_point() {
     assert_eq!(pixel_to_point((100,100), (25, 75), 
                               (-1.0, 1.0), (1.0, -1.0)), 
         (-0.5, -0.5));
+}
+
+/// Parse the string `s` as a coordinate pair, like `"400x600"` or `"1.0,0.5"`.
+///
+/// Specifically, `s` should have the form <left><sep><right>, where <sep> is
+/// the character given by the `separator` argument, and <left> and <right> are both
+/// strings that can be parsed by `T::from_str`.
+///
+/// If `s` has the proper form, return `Some<(x, y)>`. If it doesn't parse
+/// correctly, return `None`.
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("",        ','), None);
+    assert_eq!(parse_pair::<i32>("10,",     ','), None);
+    assert_eq!(parse_pair::<i32>(",10",     ','), None);
+    assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<i32>("0.5x",    ','), None);
+    assert_eq!(parse_pair::<i32>("0.5x1.5", ','), Some((0.5, 1.5)));
 }

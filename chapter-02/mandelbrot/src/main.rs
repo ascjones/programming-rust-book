7251ffa2880786536d9261e9670c59efa7d968a6
Andrew Jones <ascjones@gmail.com>
2017-05-20 11:37:02 +0100

    generate madelbrot concurrrently

--- chapter-02/mandelbrot/src/main.rs
+++ chapter-02/mandelbrot/src/main.rs
@@ -1,5 +1,6 @@
 extern crate num;
 extern crate image;
+extern crate crossbeam;
 
 use std::io::Write;
 use std::fs::File;
@@ -31,7 +32,26 @@ fn main() {
     
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    // render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = 8;
+    let band_rows = bounds.1 / threads + 1;
+    
+    crossbeam::scope(|scope| {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(band_rows * bounds.0).collect();
+        for (i, band) in bands.into_iter().enumerate() {
+            let top = band_rows * i;
+            let height = band.len() / bounds.0;
+            let band_bounds = (bounds.0, height);
+            let band_upper_left = pixel_to_point(bounds, (0, top), 
+                                                 upper_left, lower_right);
+            let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), 
+                                                  upper_left, lower_right);
+            
+            scope.spawn(move || {
+                render(band, band_bounds, band_upper_left, band_lower_right);
+            });
+        }
+    });
 
     write_bitmap(&args[1], &pixels, bounds)
         .expect("error writing PNG file");

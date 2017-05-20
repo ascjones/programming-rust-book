4ff9c6fc0831b5ed147b6ca09c762ef27cc3402a
Andrew Jones <ascjones@gmail.com>
2017-05-20 08:18:15 +0100

    implement write_bitmap

--- chapter-02/mandelbrot/src/main.rs
+++ chapter-02/mandelbrot/src/main.rs
@@ -1,27 +1,9 @@
 extern crate num;
+
 use num::Complex;
 
 fn main() {
     println!("Hello, world!");
-}
-
-/// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
-/// iterations to decide.
-///
-/// If `c` is not a member, return `Some(i)`, where `i` is the number of
-/// iterations it took for `c` to leave the circle of radius two centered on the
-/// origin. If `c` seems to be a member (more precisely, if we reached the
-/// iteration limit without being able to prove that `c` is not a member),
-/// return `None`.
-fn escapes(c: Complex<f64>, limit: u32) -> Option<u32> {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    for i in 0..limit {
-        z = z * z + c;
-        if z.norm_sqr() > 4.0 {
-            return Some(i);
-        }
-    }
-    return None;
 }
 
 /// Render a rectangle of the Mandelbrot set into a buffer of pixels.
@@ -49,6 +31,26 @@ fn render(pixels: &mut [u8],
         }
     }
 }
+
+/// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
+/// iterations to decide.
+///
+/// If `c` is not a member, return `Some(i)`, where `i` is the number of
+/// iterations it took for `c` to leave the circle of radius two centered on the
+/// origin. If `c` seems to be a member (more precisely, if we reached the
+/// iteration limit without being able to prove that `c` is not a member),
+/// return `None`.
+fn escapes(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+    return None;
+}
+
 
 /// Return the point on the complex plane corresponding to a given pixel in the
 /// bitmap.
